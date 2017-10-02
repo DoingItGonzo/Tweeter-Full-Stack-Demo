c9f27e1d33bf62c756b8a2084ae6e11933686372
unknown <mrollberg@ymail.com>
2017-10-02 14:25:55 -0500

    Merged master with my branch and fixed app js from before

--- angular/js/app.js
+++ angular/js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 angular.module('tweetApp', ['ui.router']).config(['$stateProvider', '$urlRouterProvider', function(stateProvider, urlRouter) {
     
     const hashtagState = {
@@ -7,22 +6,14 @@ angular.module('tweetApp', ['ui.router']).config(['$stateProvider', '$urlRouterP
         component: 'hashtagComponent'
     }
 
-    stateProvider.state(hashtagState);
-
-    urlRouter.otherwise('/tags');
-    
-=======
-angular.module('tweetApp', ['ui.router']).config(['$stateProvider', '$urlRouterProvider', function (stateProvider, urlRouter) {
-
     const testUserState = {
         name: 'testUser',
         url: '/testUser',
         component: 'testUserComponent'
     }
 
+    stateProvider.state(hashtagState);
     stateProvider.state(testUserState);
 
     urlRouter.otherwise('/testUser');
-
->>>>>>> master
 }]);
