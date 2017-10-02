d06cb2e26c538290dceae7bfcba1a556b454cceb
unknown <mrollberg@ymail.com>
2017-10-02 12:55:27 -0500

    All user endpoints done and tested

--- angular/js/app.js
+++ angular/js/app.js
@@ -1,4 +1,13 @@
-angular.module('tweetApp', ['ui.router']).config(['$stateProvider', '$urlRouterProvider', function(stateProvider, urlRouter) {
-    
-    
+angular.module('tweetApp', ['ui.router']).config(['$stateProvider', '$urlRouterProvider', function (stateProvider, urlRouter) {
+
+    const testUserState = {
+        name: 'testUser',
+        url: '/testUser',
+        component: 'testUserComponent'
+    }
+
+    stateProvider.state(testUserState);
+
+    urlRouter.otherwise('/testUser');
+
 }]);
