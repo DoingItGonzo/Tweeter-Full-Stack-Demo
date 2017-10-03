3a82baf37a48675329611c644009a065bbe2f832
DoingItGonzo <travis.pettrey@gmail.com>
2017-10-03 10:37:36 -0500

    cleaned up file structure. Added global service

---
+++ angular/js/components/globalService.js
@@ -0,0 +1,16 @@
+angular.module('tweetApp').service('globalService', [function () {
+    
+
+this.primaryUser = {}
+this.primaryUser.credentials = {}
+this.primaryUser.profile = {}
+
+this.primaryUser.credentials.username
+this.primaryUser.credentials.password
+
+this.primaryUser.profile.email
+this.primaryUser.profile.firstName
+this.primaryUser.profile.lastName
+this.primaryUser.profile.phone
+
+}])
