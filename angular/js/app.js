b39522a0f2c192d08214ccccbaab1ea80c79185a
DoingItGonzo <travis.pettrey@gmail.com>
2017-10-03 09:56:02 -0500

    added settings files

--- angular/js/app.js
+++ angular/js/app.js
@@ -15,25 +15,26 @@ angular.module('tweetApp', ['ui.router']).config(['$stateProvider', '$urlRouterP
         url: '/validate',
         component: 'validateComponent'
     }
-
     const signInSignUp = {
         name: 'signInSignUp',
         url: '/signInSignUp',
         component: 'signInSignUpComponent'
     }
-
     const signIn = {
         name: 'signIn',
         url: '/signIn',
         component: 'signInComponent'
     }
-
     const signUp = {
         name: 'signUp',
         url: '/signUp',
         component: 'signUpComponent'
     }
-
+    const settingState = {
+        name: 'settings',
+        url: '/settings',
+        component: 'settingComponent'
+    }
     const tweetState = {
         name: 'tweets',
         url: '/tweets',
@@ -41,12 +42,13 @@ angular.module('tweetApp', ['ui.router']).config(['$stateProvider', '$urlRouterP
       }
 
     stateProvider.state(testUserState)
-    stateProvider.state(validateState)
-    stateProvider.state(hashtagState)  
-    stateProvider.state(signInSignUp)
-    stateProvider.state(signIn)
-    stateProvider.state(signUp)
     stateProvider.state(tweetState)
+    stateProvider.state(settingState);
+    stateProvider.state(validateState);
+    stateProvider.state(hashtagState);  
+    stateProvider.state(signInSignUp);
+    stateProvider.state(signIn);
+    stateProvider.state(signUp);
 
     urlRouter.otherwise('/signInSignUp')
 }])
