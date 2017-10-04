dd35b27d1d9e4d2d08a60f4881be919411590a72
DoingItGonzo <travis.pettrey@gmail.com>
2017-10-04 12:11:04 -0500

    return home navbar dropdown button working. full navbar functionality

--- angular/js/components/navbar/navbarController.js
+++ angular/js/components/navbar/navbarController.js
@@ -1,12 +1,33 @@
-angular.module('tweetApp').controller('navbarController', ['globalService', '$state', function(globalService, state) {
+angular.module('tweetApp').controller('navbarController', ['globalService', '$state', function (globalService, state) {
 
     this.globalService = globalService
 
     this.userSearch
 
+    this.searchedUser = {}
+    this.searchedUser.credentials = {}
+    this.searchedUser.profile = {}
+
+    this.searchedUser.credentials.username
+    this.searchedUser.credentials.password
+
+    this.searchedUser.profile.email
+    this.searchedUser.profile.firstName
+    this.searchedUser.profile.lastName
+    this.searchedUser.profile.phone
+
     this.search = () => {
         if (this.userSearch.startsWith("@")) {
-            globalService.userService.getUser(this.userSearch.substring(1))
+            globalService.userService.getUser(this.userSearch.substring(1)).then((done) => {
+                console.log(done)
+                this.searchedUser = done.data
+                state.go('userPage', {
+                    credentials: {
+                        username: this.searchedUser.username,
+                        password: undefined
+                    }
+                })
+            })
         }
         else if (this.userSearch.startsWith("#")) {
             console.log(this.userSearch)
@@ -14,6 +35,18 @@ angular.module('tweetApp').controller('navbarController', ['globalService', '$st
                 label: this.userSearch.substring(1)
             })
         }
+    }
+    this.homePage = () => {
+        globalService.userService.getUser(globalService.primaryUser.credentials.username).then((done) => {
+            console.log(done)
+            this.searchedUser = done.data
+            state.go('userPage', {
+                credentials: {
+                    username: this.searchedUser.username,
+                    password: undefined
+                }
+            })
+        })
     }
 
 }])
