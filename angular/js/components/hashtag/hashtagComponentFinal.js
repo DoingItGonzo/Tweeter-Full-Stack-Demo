a4a2ad4e18d45a3f597f0c3639741d611c0e2264
markhdwight <markhdwight@gmail.com>
2017-10-05 16:42:21 -0500

    removed/renamed hashtag files to remove old test files

--- angular/js/components/hashtag/hashtagComponentFinal.js
+++
@@ -1,7 +0,0 @@
-angular.module('tweetApp').component('hashtagComponentFinal', {
-    templateUrl: 'js/components/hashtag/hashtagTemplateFinal.html',
-    controller: 'hashtagController',
-    bindings:{
-        hashtag : '='
-    }
-})
